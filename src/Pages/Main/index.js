b973192c3ceabe688dfecd925401158fa29bebbd
zira-el <niltonmenezes@live.com>
2021-10-25 15:50:56 -0300

    Criaçao dos destaques

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './styles.css';
-import { FaSearch, FaRegBell } from "react-icons/fa";
+import { FaSearch, FaRegBell, FaPlay } from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
+
       <header className="header">
         <div className='container flex-row content-between items-center'>
-
           <div className='left flex-row content-between'>
             <div className='logo'>
               <h3>NIMEFLIX</h3>
@@ -25,9 +25,22 @@ function App() {
             <FaRegBell />
             <div className='user-image'></div>
           </div>
-
         </div>
       </header>
+
+      <main>
+        <section className='highlights'>
+          <div className='container-info flex-column content-center items-start'>
+            <h1 className='title'>Dragon Ball</h1>
+            <span className='info'>Informações do anime</span>
+            <button type='button' className='flex-row items-center' >
+              <FaPlay className='margin'/>
+              ASSISTIR AGORA
+              </button>
+          </div>
+        </section>
+      </main>
+
     </div>
   );
 }
